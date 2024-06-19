912e3cec9e3b639bf8b617522f54638d2b23131d
천영성 <hokoro123@gmail.com>
2024-06-19 14:19:59 +0900

    Spring boot 연습 1. CRUD 2. DB 연동 3. 포트 설정

---
+++ src/main/java/com/example/spring/crud_post/CrudPostApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.crud_post;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudPostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudPostApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spring/crud_post/CrudPostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.crud_post;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudPostApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
